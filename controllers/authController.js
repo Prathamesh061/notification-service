29b777980da82015f2a6136a045a717832b735da
Prathamesh Lakhpati <prathmeshlakhpaty@gmail.com>
2025-07-25 02:14:52 +0530

    rm log

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,6 @@ exports.signup = async (req, res) => {
         await user.save();
         res.status(201).json({ message: 'User registered successfully' });
     } catch (err) {
-        console.log(err)
         res.status(500).json({ message: 'Server error' });
     }
 };
